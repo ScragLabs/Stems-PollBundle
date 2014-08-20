3f800cd0834251777ec21998a2ef5c2d4ede342a
Stephen Wilkinson <du_monde@hotmail.com>
2014-08-20 09:50:48 +0000

    Added IP vote check to poll widget

--- Controller/WidgetController.php
+++ Controller/WidgetController.php
@@ -10,17 +10,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller,
 class WidgetController extends Controller
 {
 	/**
-	 * Renders a poll widget
+	 * Renders a poll in a widget
 	 *
 	 * @param  integer 	$id 	The ID of the poll to render
+	 * @param  Request  $request
 	 */
-	public function renderPollAction($id)
+	public function renderPollAction(Request $request, $id)
 	{
-		// get the latest blog post
+		// Get the requested poll
 		$em   = $this->getDoctrine()->getEntityManager();
 		$poll = $em->getRepository('StemsPollBundle:Poll')->find($id);
 
+		// Check if the user has already voted from their IP
+		$ip    = $request->getClientIp();
+		$voted = $em->getRepository('StemsPollBundle:Poll')->checkIpAlreadyVoted($ip, $poll);
+
 		return $this->render('StemsPollBundle:Widget:renderPoll.html.twig', array(
+			'voted' => $voted,
 			'poll' 	=> $poll,
 		));
 	}
